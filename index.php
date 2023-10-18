25308646869e48b414689e23846cc6e147981673
elhazaa <148293518+elhazaa@users.noreply.github.com>
2023-10-17 17:04:07 -0700

    Update index.php

--- index.php
+++ index.php
@@ -45,7 +45,7 @@
         <p>El cómputo en la nube es un modelo que permite el acceso a recursos de cómputo, como servidores, almacenamiento, bases de datos, redes y software a través de Internet. Esto permite a las organizaciones escalar y utilizar recursos de manera más eficiente, reduciendo costos y mejorando la flexibilidad. En resumen, el cómputo en la nube ofrece servicios tecnológicos bajo demanda.</p>
     </div>
     <footer>
-        &copy; 2023 Cómputo en la Nube
+        &copy; 2023 Cómputo en la Nube, Avila Perez Jauiel, Acosta FLores Jose Hazael, Cruz Benitez Carlos Rene.
     </footer>
 </body>
 </html>
